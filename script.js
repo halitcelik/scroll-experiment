c1ab6df8ad52579c0be0a34009d50c42fbb1f5e1
Halit Celik <halit@cruncher.ch>
2021-07-01 16:58:12 +0200

    some fiddling

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 var sections = document.querySelectorAll("section");
 
 document.addEventListener("scroll", (e) => {
@@ -7,13 +6,13 @@ document.addEventListener("scroll", (e) => {
     console.log(scrollPos)
 
     var offset = document.querySelector("section").offsetTop;
-    if(document.documentElement.scrollTop > (offset - 16)) {
+    if (document.documentElement.scrollTop > (offset - 16)) {
         sections.forEach((s) => {
             document.getElementById("content").classList.add("stick");
             s.style.height = "100vh";
             s.style.overflow = "scroll";
         });
-    }else if(document.documentElement.scrollTop < 100){
+    } else if (document.documentElement.scrollTop < 100) {
         sections.forEach((s) => {
             document.getElementById("content").classList.remove("stick");
             s.style.height = "auto";
@@ -23,22 +22,37 @@ document.addEventListener("scroll", (e) => {
 
 })
 
+var lastOrder;
+var lastOrdered;
 
-
+window.addEventListener("click", (e) => {
+    var t = e.target;
+    if (t.classList.contains("item-image")) {
+        if (t.parentElement.classList.contains("full")) {
+            t.parentElement.classList.remove("full");
+            lastOrder = t.style.order;
+            lastOrdered = t;
+            t.style.order = Math.floor(parseInt(lastOrder, 10) / 3) - 1;
+        } else {
+            t.style.order = lastOrder;
+            t.parentElement.classList.add("full");
+        }
+    }
+})
 
 // left: 37, up: 38, right: 39, down: 40,
 // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
-var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
 
 function preventDefault(e) {
-  e.preventDefault();
+    e.preventDefault();
 }
 
 function preventDefaultForScrollKeys(e) {
-  if (keys[e.keyCode]) {
-    preventDefault(e);
-    return false;
-  }
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
 }
 /*
 var wheelOpt = { passive: true };
@@ -57,16 +71,15 @@ function disableScroll() {
 
 
 function preventScrollWheel(event) {
-  if (typeof event.cancelable !== 'boolean' || event.cancelable) {
-    // The event can be canceled, so we do so.
-    event.preventDefault();
-  } else {
-    // The event cannot be canceled, so it is not safe
-    // to call preventDefault() on it.
-    console.warn(`The following event couldn't be canceled:`);
-    console.dir(event);
-  }
+    if (typeof event.cancelable !== 'boolean' || event.cancelable) {
+        // The event can be canceled, so we do so.
+        event.preventDefault();
+    } else {
+        // The event cannot be canceled, so it is not safe
+        // to call preventDefault() on it.
+        console.warn(`The following event couldn't be canceled:`);
+        console.dir(event);
+    }
 }
 
 document.addEventListener('wheel', preventScrollWheel);
-
